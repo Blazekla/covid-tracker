a50a43f52b6d09f2c49b655f42cba8db5a912bda
Luis Cristo <cristlui4@gmail.com>
2020-08-28 16:26:30 -0500

    added line recharts example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import LineExample from "./components/LineChartExample";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <LineExample />
       </header>
     </div>
   );
