d8adfb887354d6962c1adfa349a47c0c03356348
Luis Cristo <cristlui4@gmail.com>
2020-09-22 17:14:33 -0500

    changed autoprefixer version to fix bug and added maincss for styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import LineChart from "./components/LineChart";
 import "./App.css";
-
+import "./assets/css/main.css";
 function App() {
   return (
     <div className="App">
+      <p class="text-xs ...">The quick brown fox ...</p>
+      <p class="text-sm ...">The quick brown fox ...</p>
+      <p class="text-base ...">The quick brown fox ...</p>
+      <p class="text-lg ...">The quick brown fox ...</p>
+      <p class="text-xl ...">The quick brown fox ...</p>
+      <p class="text-2xl ...">The quick brown fox ...</p>
+      <p class="text-3xl ...">The quick brown fox ...</p>
+      <p class="text-4xl ">The quick brown fox ...</p>
+      <p class="text-5xl ...">The quick brown fox ...</p>
+      <p class="text-6xl ...">The quick brown fox ...</p>
       <header className="App-header">
         <LineChart />
       </header>
