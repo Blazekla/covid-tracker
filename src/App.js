abc6b651827c297ae793f9bc519398d60cf3cf5a
Luis Cristo <cristlui4@gmail.com>
2020-09-23 15:22:03 -0500

    more  tailwindcss testing

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import LineChart from "./components/LineChart";
 import "./assets/css/main.css";
 import Tailwind from "./components/tailwindexample";
+import Menu from "./components/selectMenu";
+import SimpleToggle from "./components/simpleToggle";
 
 function App() {
   return (
     <div className="flex flex-col  bg-indigo-900">
-      <header className="flex justify-between items-center bg-gray-600 px-8">
+      <header className="flex justify-between items-center bg-gray-700 px-8">
         <h1 className="text-4xl ">Covid Tracker</h1>
         <p>By State</p>
       </header>
       <main>
+        {/* <Menu /> */}
+        <SimpleToggle />
         <LineChart />
-        <Tailwind />
+        {/* <Tailwind /> */}
       </main>
       <footer>
-        <p className="text-2xl bg-gray-600">Footer Here</p>
+        <p className="text-2xl bg-gray-700">Footer Here</p>
       </footer>
     </div>
   );
