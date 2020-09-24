db47f827dfc5225fc0d6f23ad2b8ca478e6e95b2
Luis Cristo <cristlui4@gmail.com>
2020-09-24 13:03:54 -0500

    added initial route paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import Layout from "./components/Layout";
 import LineChart from "./components/LineChart";
-
 import SimpleToggle from "./components/simpleToggle";
 
 function App() {
   return (
     <Layout>
-      <SimpleToggle />
-      <LineChart />
+      <Link to="/us">US Data</Link>
+      <Link to="/state">State Data</Link>
+      <Switch>
+        <Route path="/us">
+          <SimpleToggle />
+        </Route>
+        <Route path="/state">
+          <LineChart />
+        </Route>
+      </Switch>
     </Layout>
   );
 }
