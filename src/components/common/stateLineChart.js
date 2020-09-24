b603798a825696436bf7c202af7333ea537a00ae
Luis Cristo <cristlui4@gmail.com>
2020-09-24 16:19:05 -0500

    code cleanup, styling and minor fix

--- src/components/common/stateLineChart.js
+++ src/components/common/stateLineChart.js
@@ -18,8 +18,7 @@ function LineChartComponent(props) {
   const [selectedState, setSelectedState] = useState("TX");
   const [selectedType, setSelectedType] = useState("newCases");
 
-  // const today = DateTime.local().minus({ day: 1 }).toFormat("LLL d");
-  const today = DateTime.local().toFormat("LLL d");
+  const today = DateTime.local().minus({ day: 1 }).toFormat("LLL d");
   useEffect(() => {
     async function fetchTotals() {
       try {
@@ -62,7 +61,7 @@ function LineChartComponent(props) {
   };
 
   return (
-    <div className="flex flex-col items-center px-12 my-12 ">
+    <div className="flex flex-col items-center my-12 ">
       <div className="p-12">
         <select value={selectedState} onChange={handleChange}>
           <option value="one" disabled>
@@ -89,72 +88,76 @@ function LineChartComponent(props) {
       ) : (
         "nothing to see here"
       )}
-      {totalCases && (
-        <ResponsiveContainer width="99%" height={300} className="bg-gray-300">
-          <LineChart
-            width={500}
-            height={500}
-            data={totalCases}
-            margin={{
-              top: 10,
-              right: 0,
-              left: 0,
-              bottom: 10,
-            }}
+      <div className="container mx-auto px-1 sm:px-4">
+        {totalCases && (
+          <ResponsiveContainer
+            width="100%"
+            height={300}
+            className="bg-gray-300"
           >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              reversed
-              // allowDataOverflow
-              dataKey="date"
-              // domain={["auto", "auto"]}
-              // domain={[0, 200]}
-              // type="category"
-              // interval="preserveStart"
-              interval="preserveStartEnd"
-              // interval="preserveEnd"
-              // interval={2}
-              ticks={[
-                // "Mar 4",
-                "Apr 1",
-                "May 1",
-                "Jun 1",
-                "Jul 1",
-                "Aug 1",
-                "Sep 1",
-                today,
-                // DateTime.local().toFormat("LLL d").valueOf(String),
-              ]}
+            <LineChart
+              data={totalCases}
+              margin={{
+                top: 10,
+                right: 10,
+                left: -10,
+                bottom: 10,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                reversed
+                // allowDataOverflow
+                dataKey="date"
+                // domain={["auto", "auto"]}
+                // domain={[0, 200]}
+                // type="category"
+                // interval="preserveStart"
+                interval="preserveStartEnd"
+                // interval="preserveEnd"
+                // interval={2}
+                ticks={[
+                  // "Mar 4",
+                  "Apr 1",
+                  "May 1",
+                  "Jun 1",
+                  "Jul 1",
+                  "Aug 1",
+                  "Sep 1",
+                  today,
+                  // DateTime.local().toFormat("LLL d").valueOf(String),
+                ]}
 
-              // tickLine={false}
-            />
-            <YAxis
-              dataKey={
-                selectedType === "newCases"
-                  ? "positiveIncrease"
-                  : "deathIncrease"
-              }
-            />
-            <Tooltip
-              itemStyle={{ color: "purple" }}
-              wrapperStyle={{ backgroundColor: "pink", color: "black" }}
-              isAnimationActive={false}
-            />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey={
-                selectedType === "newCases"
-                  ? "positiveIncrease"
-                  : "deathIncrease"
-              }
-              name={`Positive Cases in ${totalCases[0].state}`}
-              stroke="#88844d"
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      )}
+                // tickLine={false}
+              />
+              <YAxis
+                dataKey={
+                  selectedType === "newCases"
+                    ? "positiveIncrease"
+                    : "deathIncrease"
+                }
+              />
+              <Tooltip
+                itemStyle={{ color: "purple" }}
+                wrapperStyle={{ backgroundColor: "pink", color: "black" }}
+                isAnimationActive={false}
+              />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey={
+                  selectedType === "newCases"
+                    ? "positiveIncrease"
+                    : "deathIncrease"
+                }
+                name={`Positive Cases in ${totalCases[0].state}`}
+                stroke="#88844d"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
+      </div>
     </div>
   );
 }
