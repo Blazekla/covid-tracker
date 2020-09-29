7e5b67f878a3d7fa274a4fc5aac3cf84940b7674
Luis Cristo <cristlui4@gmail.com>
2020-09-29 15:31:27 -0500

    added toggle button code for testing

--- src/components/State/stateLineChart.js
+++ src/components/State/stateLineChart.js
@@ -8,6 +8,9 @@ function LineChartComponent(props) {
   const [totalCases, setTotalCases] = useState(null);
   const [selectedState, setSelectedState] = useState("TX");
   const [selectedType, setSelectedType] = useState("newCases");
+  // const [lineChart, setLineChart] = useState(false);
+  // const [barChart, setBarChart] = useState(false);
+  // const [areaChart, setAreaChart] = useState(false);
 
   const today = DateTime.local().minus({ day: 1 }).toFormat("LLL d");
   useEffect(() => {
@@ -70,7 +73,23 @@ function LineChartComponent(props) {
           <option value="newDeaths">New Deaths</option>
         </select>
       </div>
-
+      {/* <div className="m-20">
+        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+          <input
+            type="checkbox"
+            name="toggle"
+            id="toggle"
+            class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+          />
+          <label
+            for="toggle"
+            class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+          ></label>
+        </div>
+        <label for="toggle" class="text-xs text-gray-700">
+          Toggle me.
+        </label>
+      </div> */}
       {totalCases ? (
         <h1 className="text-white p-8">Totals in {selectedState}</h1>
       ) : (
