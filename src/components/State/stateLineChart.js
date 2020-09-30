22be70933ca3c8239d44e3023eb8cd3a1df1f0ef
Luis Cristo <cristlui4@gmail.com>
2020-09-30 13:12:56 -0500

    removed testing toggle button

--- src/components/State/stateLineChart.js
+++ src/components/State/stateLineChart.js
@@ -73,23 +73,6 @@ function LineChartComponent(props) {
           <option value="newDeaths">New Deaths</option>
         </select>
       </div>
-      {/* <div className="m-20">
-        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-          <input
-            type="checkbox"
-            name="toggle"
-            id="toggle"
-            class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
-          />
-          <label
-            for="toggle"
-            class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
-          ></label>
-        </div>
-        <label for="toggle" class="text-xs text-gray-700">
-          Toggle me.
-        </label>
-      </div> */}
       {totalCases ? (
         <h1 className="text-white p-8">Totals in {selectedState}</h1>
       ) : (
