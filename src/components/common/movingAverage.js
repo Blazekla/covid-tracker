50035a3a668bf449a6ac5d561da3520c95a69d01
Luis Cristo <cristlui4@gmail.com>
2020-10-06 15:42:11 -0500

    added initial working code for moving average function

--- src/components/common/movingAverage.js
+++ src/components/common/movingAverage.js
@@ -1,8 +1,50 @@
 function movingAverage(value) {
-  const movingAverageArr = [...value];
-  console.log("value passed: ", value);
+  const copyOfArray = [...value];
 
-  return movingAverageArr;
+  function test(param) {
+    console.log("it worked!", param);
+  }
+
+  const newMappedArr = copyOfArray.map((element, index) => {
+    let N = 7;
+
+    if (N > index + 1) {
+      N = index + 1;
+    }
+
+    let sum = 0;
+    for (let x = 0; x < N; x++) {
+      //   console.log(
+      //     "inside loop: ",
+      //     index,
+      //     copyOfArray[index - x].positiveIncrease
+      //   );
+      if (index === 215) {
+        console.log("sum at x: ", x, " ", sum);
+        console.log(
+          "array value: ",
+          index - x,
+          " value of array: ",
+          copyOfArray[index - x].positiveIncrease
+        );
+      }
+      sum += copyOfArray[index - x].positiveIncrease;
+    }
+    const result = Math.round(sum / N);
+    // const result = sum / N;
+    if (index === 215) {
+      console.log("sum result: ", sum);
+    }
+    console.log("index: ", index, " result: ", result);
+    // test(copyOfArray[index]);
+    //     /N
+    return result;
+  });
+
+  console.log("value passed: ", copyOfArray.length);
+  console.log("value changed new array: ", newMappedArr);
+
+  return newMappedArr;
 }
 
 export default movingAverage;
