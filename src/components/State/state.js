0a0851610f1c45216a97d25e19962146ba48238b
Luis Cristo <cristlui4@gmail.com>
2020-12-04 17:30:24 -0600

    added temaple for table data in state component

--- src/components/State/state.js
+++ src/components/State/state.js
@@ -8,6 +8,30 @@ function State() {
         Here is the state data
       </p> */}
       <StateComposedChart />
+      <div className="px-16 flex flex-wrap">
+        <h2>Table</h2>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>New Tests</th>
+            <th>Cases</th>
+            <th>Negative PCR Test</th>
+            <th>Currently hopitalized</th>
+            <th>Deaths</th>
+            <th>Total Test Results</th>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
