79a297ebc479c3384a6ac694509b0a99b2607168
Luis Cristo <cristlui4@gmail.com>
2020-12-12 22:28:28 -0600

    refactored chartModal and added styling and functionality

--- src/components/State/stateChart.js
+++ src/components/State/stateChart.js
@@ -5,7 +5,8 @@ import React, {
 // import axios from "axios";
 import { DateTime } from "luxon";
 import { stateLabelValues } from "../../data/stateLabel";
-import ComposedChart from "../common/composedChart";
+// import ComposedChart from "../common/composedChart";
+import ChartModal from "../common/chartModal";
 import SmallComposedChart from "../common/smallComposedChart";
 
 function StateChart({ totalCases, selectedState, handleStateChange, display }) {
@@ -67,92 +68,20 @@ function StateChart({ totalCases, selectedState, handleStateChange, display }) {
         </div>
       </div>
       {toggle ? (
-        <div className="h-screen w-screen left-0 top-0 fixed z-50 flex flex-col justify-center bg-black bg-opacity-30">
-          <div className="flex flex-col items-center my-12 bg-red-900">
-            {totalCases ? (
-              <h1 className="text-white p-4">Totals in {selectedState}</h1>
-            ) : (
-              <h1 className="text-white p-4">Loading Data</h1>
-            )}
-            <button
-              onClick={handleToggle}
-              className="text-white border-green-900 border p-2"
-            >
-              X
-            </button>
-            <div className="flex flex-wrap justify-center">
-              <div className="p-2 sm:p-8">
-                <select
-                  value={selectedState}
-                  onChange={handleStateChange}
-                  className="rounded-full px-1"
-                >
-                  <option value="one" disabled>
-                    Choose a State
-                  </option>
-                  {stateLabelValues.map((state) => {
-                    return (
-                      <option key={state.label} value={state.value}>
-                        {state.label}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-              <div className="p-2 sm:p-8">
-                <select
-                  value={selectedType}
-                  onChange={handleTypeChange}
-                  className="rounded-full px-1"
-                >
-                  <option value="newCases">New Cases</option>
-                  <option value="newDeaths">New Deaths</option>
-                </select>
-              </div>
-              <div className="text-white p-2 sm:p-8 flex flex-wrap justify-center">
-                <div>
-                  <label className="m-4" htmlFor="barchart">
-                    Bar Chart:
-                  </label>
-                  <input
-                    type="checkbox"
-                    name="barchartcheck"
-                    id="barchart"
-                    checked={barChart}
-                    onChange={handleBarChartChange}
-                  />
-                </div>
-                <div>
-                  <label className="m-4" htmlFor="7dayaverage">
-                    7-Day Average:
-                  </label>
-                  <input
-                    type="checkbox"
-                    name="lineaveragechart"
-                    id="7dayaverage"
-                    checked={lineChart}
-                    onChange={handleLineChartChange}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="container mx-auto px-2 sm:px-4 mb-16">
-              {totalCases && (
-                <ComposedChart
-                  totalCases={totalCases}
-                  today={today}
-                  selectedType={selectedType}
-                  location={selectedState}
-                  lineToggle={lineChart}
-                  barToggle={barChart}
-                  heightInput={400}
-                  minified={true}
-                />
-              )}
-            </div>
-          </div>
-        </div>
+        <ChartModal
+          totalCases={totalCases}
+          selectedState={selectedState}
+          handleToggle={handleToggle}
+          handleStateChange={handleStateChange}
+          stateLabelValues={stateLabelValues}
+          selectedType={selectedType}
+          handleTypeChange={handleTypeChange}
+          barChart={barChart}
+          handleBarChartChange={handleBarChartChange}
+          lineChart={lineChart}
+          handleLineChartChange={handleLineChartChange}
+          today={today}
+        />
       ) : null}
     </>
   );
---
+++ src/components/common/chartModal.js
@@ -0,0 +1,120 @@
+import React, { useEffect } from "react";
+import ComposedChart from "./composedChart";
+function ChartModal({
+  totalCases,
+  selectedState,
+  handleToggle,
+  handleStateChange,
+  stateLabelValues,
+  selectedType,
+  handleTypeChange,
+  barChart,
+  handleBarChartChange,
+  lineChart,
+  handleLineChartChange,
+  today,
+}) {
+  useEffect(() => {
+    // effect
+    return () => {
+      //   cleanup;
+    };
+  }, []);
+  return (
+    <div
+      //   onClick={handleToggle}
+      className="h-screen w-screen left-0 top-0 fixed z-30 flex flex-col justify-center "
+    >
+      <div
+        className="bg-black bg-opacity-30 z-40 left-0 top-0 right-0 bottom-0 absolute"
+        onClick={handleToggle}
+      ></div>
+      <div className="flex flex-col items-center my-12 bg-red-900 z-50">
+        {totalCases ? (
+          <h1 className="text-white p-4">Totals in {selectedState}</h1>
+        ) : (
+          <h1 className="text-white p-4">Loading Data</h1>
+        )}
+        <button
+          onClick={handleToggle}
+          className="text-white border-green-900 border p-2"
+        >
+          X
+        </button>
+        <div className="flex flex-wrap justify-center">
+          <div className="p-2 sm:p-8">
+            <select
+              value={selectedState}
+              onChange={handleStateChange}
+              className="rounded-full px-1"
+            >
+              <option value="one" disabled>
+                Choose a State
+              </option>
+              {stateLabelValues.map((state) => {
+                return (
+                  <option key={state.label} value={state.value}>
+                    {state.label}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="p-2 sm:p-8">
+            <select
+              value={selectedType}
+              onChange={handleTypeChange}
+              className="rounded-full px-1"
+            >
+              <option value="newCases">New Cases</option>
+              <option value="newDeaths">New Deaths</option>
+            </select>
+          </div>
+          <div className="text-white p-2 sm:p-8 flex flex-wrap justify-center">
+            <div>
+              <label className="m-4" htmlFor="barchart">
+                Bar Chart:
+              </label>
+              <input
+                type="checkbox"
+                name="barchartcheck"
+                id="barchart"
+                checked={barChart}
+                onChange={handleBarChartChange}
+              />
+            </div>
+            <div>
+              <label className="m-4" htmlFor="7dayaverage">
+                7-Day Average:
+              </label>
+              <input
+                type="checkbox"
+                name="lineaveragechart"
+                id="7dayaverage"
+                checked={lineChart}
+                onChange={handleLineChartChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="container mx-auto px-2 sm:px-4 mb-16">
+          {totalCases && (
+            <ComposedChart
+              totalCases={totalCases}
+              today={today}
+              selectedType={selectedType}
+              location={selectedState}
+              lineToggle={lineChart}
+              barToggle={barChart}
+              heightInput={400}
+              minified={true}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChartModal;
