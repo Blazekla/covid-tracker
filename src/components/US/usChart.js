d5139cacf3a84248b876bb3ca02a6b59be2bf678
Luis Cristo <cristlui4@gmail.com>
2020-12-30 17:36:50 -0600

    adjusted uschart skeleton loader to match theme color

--- src/components/US/usChart.js
+++ src/components/US/usChart.js
@@ -101,18 +101,18 @@ function USLineChart({ timeframe }) {
           <div className="p-4  w-full mx-auto" style={{ height: "400px" }}>
             <div className="animate-pulse h-full bg-blue-100 w-full">
               <div className="flex rounded w-full h-full justify-between px-8 mx-auto items-end space-x-1">
-                <div className="bg-blue-200 w-1/12 h-1/6"></div>
-                <div className="bg-blue-200 w-1/12 h-4/6"></div>
-                <div className="bg-blue-200 w-1/12 h-3/6"></div>
-                <div className="bg-blue-200 w-1/12 h-5/6"></div>
-                <div className="bg-blue-200 w-1/12 h-4/6"></div>
-                <div className="bg-blue-200 w-1/12 h-4/6"></div>
-                <div className="bg-blue-200 w-1/12 h-2/6"></div>
-                <div className="bg-blue-200 w-1/12 h-5/6"></div>
-                <div className="bg-blue-200 w-1/12 h-4/6"></div>
-                <div className="bg-blue-200 w-1/12 h-4/6"></div>
-                <div className="bg-blue-200 w-1/12 h-2/6"></div>
-                <div className="bg-blue-200 w-1/12 h-5/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-1/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-3/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
+                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
               </div>
             </div>
           </div>
