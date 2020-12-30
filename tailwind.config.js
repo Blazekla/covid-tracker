cf05e6baca8969ed4980ac26c339300e489698e3
Luis Cristo <cristlui4@gmail.com>
2020-12-30 15:55:46 -0600

    added primary and secondary theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,24 @@ module.exports = {
   purge: ["./src/**/*.js", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      primary: "#282c34",
-    }),
+    // backgroundColor: (theme) => ({
+    //   ...theme("colors"),
+    //   // primary: "#282c34",
+    // }),
     extend: {
+      colors: {
+        // amberrose: colors.amber,
+        primary: {
+          light: "#377564",
+          main: "#00493a",
+          dark: "#002214",
+        },
+        secondary: {
+          light: "#e34d2c",
+          main: "#aa1200",
+          dark: "#740000",
+        },
+      },
       textColor: {
         primary: "#3490dc",
         secondary: "#ffed4a",
