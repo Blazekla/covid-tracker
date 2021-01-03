da9dae2839b02dca6d46c4526fbca3eed9709083
Luis Cristo <cristlui4@gmail.com>
2021-01-03 13:52:45 -0600

    added table to the uschart component

--- src/components/US/usChart.js
+++ src/components/US/usChart.js
@@ -1,21 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import { DateTime } from "luxon";
 import ComposedChart from "../common/composedChart";
+import USTable from "./usTable";
 
-function USLineChart({ timeframe }) {
+function USChart({ timeframe }) {
   const [totalCases, setTotalCases] = useState(null);
   const [selectedType, setSelectedType] = useState("newCases");
   const [lineChart, setLineChart] = useState(false);
+  const [skeleton, setSkeleton] = useState(true);
 
   const today = DateTime.local().minus({ day: 1 }).toFormat("LLL d");
   useEffect(() => {
     async function fetchTotals() {
       try {
+        setSkeleton(true);
         const data = await axios.get(
           `https://api.covidtracking.com/v1/us/${timeframe}.json`
         );
 
+        setSkeleton(false);
         data.data.forEach((item) => {
           item.date = DateTime.fromISO(item.date).toFormat("LLL d");
         });
@@ -36,74 +40,125 @@ function USLineChart({ timeframe }) {
     setLineChart(!lineChart);
   };
 
-  return (
-    <div className="flex flex-col items-center my-12 ">
-      {totalCases ? (
-        <h1 className="text-white p-8 text-lg">Totals in the US</h1>
-      ) : (
-        <h1 className="text-white p-8">Loading Data</h1>
-      )}
-      <div className="flex flex-wrap justify-center">
-        <div className="p-2 sm:p-8">
-          <select
-            value={selectedType}
-            onChange={handleTypeChange}
-            className="rounded-full px-1"
-          >
-            <option value="newCases">New Cases</option>
-            <option value="newDeaths">New Deaths</option>
-          </select>
-        </div>
-        <div className="text-white p-2 sm:p-8 flex flex-wrap justify-center">
-          <div>
-            <label className="m-4" htmlFor="7dayaverage">
-              7-Day Average:
-            </label>
-            <input
-              type="checkbox"
-              name="lineaveragechart"
-              id="7dayaverage"
-              checked={lineChart}
-              onChange={handleLineChartChange}
-            />
-          </div>
-        </div>
-      </div>
+  // ****** //
+  const [sortedField, setSortedField] = useState(null);
+  const handleTableHeaderClick = (name) => {
+    requestSort(name);
+  };
 
-      <div className="container mx-auto px-2 sm:px-4 mb-16">
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortedField &&
+      sortedField.key === key &&
+      sortedField.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortedField({ key, direction });
+  };
+  let sortedData = useMemo(() => {
+    if (totalCases && sortedField !== null) {
+      const data = [...totalCases];
+      data.sort((a, b) => {
+        if (a[sortedField.key] < b[sortedField.key]) {
+          return sortedField.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortedField.key] > b[sortedField.key]) {
+          return sortedField.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+      return data;
+    } else if (totalCases) {
+      const data = [...totalCases];
+      return data.reverse();
+    } else {
+      return null;
+    }
+  }, [sortedField, totalCases]);
+
+  // ****** //
+
+  return (
+    <>
+      <div className="flex flex-col items-center my-12 ">
         {totalCases ? (
-          <ComposedChart
-            totalCases={totalCases}
-            today={today}
-            selectedType={selectedType}
-            location="US"
-            lineToggle={lineChart}
-            barToggle={true}
-            minified={true}
-          />
+          <h1 className="text-white p-8 text-lg">Totals in the US</h1>
         ) : (
-          <div className="p-4  w-full mx-auto" style={{ height: "400px" }}>
-            <div className="animate-pulse h-full bg-blue-100 w-full">
-              <div className="flex rounded w-full h-full justify-between px-8 mx-auto items-end space-x-1">
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-1/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-3/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
-                <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
-              </div>
+          <h1 className="text-white p-8">Loading Data</h1>
+        )}
+        <div className="flex flex-wrap justify-center">
+          <div className="p-2 sm:p-8">
+            <select
+              value={selectedType}
+              onChange={handleTypeChange}
+              className="rounded-full px-1"
+            >
+              <option value="newCases">New Cases</option>
+              <option value="newDeaths">New Deaths</option>
+            </select>
+          </div>
+          <div className="text-white p-2 sm:p-8 flex flex-wrap justify-center">
+            <div>
+              <label className="m-4" htmlFor="7dayaverage">
+                7-Day Average:
+              </label>
+              <input
+                type="checkbox"
+                name="lineaveragechart"
+                id="7dayaverage"
+                checked={lineChart}
+                onChange={handleLineChartChange}
+              />
             </div>
           </div>
-        )}
+        </div>
+
+        <div className="container mx-auto px-2 sm:px-4 mb-16">
+          {totalCases ? (
+            <ComposedChart
+              totalCases={totalCases}
+              today={today}
+              selectedType={selectedType}
+              location="US"
+              lineToggle={lineChart}
+              barToggle={true}
+              minified={true}
+            />
+          ) : (
+            <div className="p-4  w-full mx-auto" style={{ height: "400px" }}>
+              <div className="animate-pulse h-full bg-blue-100 w-full">
+                <div className="flex rounded w-full h-full justify-between px-8 mx-auto items-end space-x-1">
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-1/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-3/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-4/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-2/6"></div>
+                  <div className="bg-primary-light bg-opacity-40 w-1/12 h-5/6"></div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className="px-4 flex flex-wrap flex-col items-center max-w-max mx-auto mb-12">
+        <USTable
+          totalCases={totalCases}
+          sortedData={sortedData}
+          sortedField={sortedField}
+          handleTableHeaderClick={handleTableHeaderClick}
+          skeleton={skeleton}
+        />
+      </div>
+    </>
   );
 }
 
-export default USLineChart;
+export default USChart;
---
+++ src/components/US/usTable.js
@@ -0,0 +1,171 @@
+import React from "react";
+
+function USTable({
+  totalCases,
+  sortedData,
+  sortedField,
+  handleTableHeaderClick,
+  skeleton,
+}) {
+  const checkSorting = (value) => {
+    if (sortedField && sortedField.key === value) {
+      if (sortedField.direction === "ascending") {
+        return (
+          <svg
+            // class="MuiSvgIcon-root jss210"
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            fill="#fff"
+            width="20"
+            height="20"
+          >
+            <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path>
+          </svg>
+        );
+      } else if (sortedField.direction === "descending") {
+        return (
+          <svg
+            // class="MuiSvgIcon-root jss210"
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            fill="#fff"
+            width="20"
+            height="20"
+          >
+            <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
+          </svg>
+        );
+      }
+    }
+
+    return null;
+  };
+
+  return (
+    <table
+      className={`table-auto border-collapse overflow-x-auto block w-full ${
+        totalCases && !skeleton ? "calculate-100" : null
+      } overflow-y-auto`}
+    >
+      <thead className="text-white">
+        <tr>
+          <th
+            className="sticky top-0 left-0 z-10 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("rawDate")}
+          >
+            Date{checkSorting("rawDate")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("positive")}
+          >
+            Total Cases{checkSorting("positive")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("positiveIncrease")}
+          >
+            New Cases{checkSorting("positiveIncrease")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("death")}
+          >
+            Total Deaths{checkSorting("death")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("deathIncrease")}
+          >
+            New Deaths{checkSorting("deathIncrease")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("totalTestResults")}
+          >
+            Total Tests{checkSorting("totalTestResults")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("totalTestResultsIncrease")}
+          >
+            New Tests{checkSorting("totalTestResultsIncrease")}
+          </th>
+          <th
+            className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
+            onClick={() => handleTableHeaderClick("hospitalizedCurrently")}
+          >
+            Currently Hospitalized{checkSorting("hospitalizedCurrently")}
+          </th>
+        </tr>
+      </thead>
+      {sortedData && !skeleton ? (
+        <tbody className="text-white">
+          {sortedData.map((cases) => {
+            return (
+              <tr
+                key={cases.date}
+                className="text-right border-4 border-white hover:bg-secondary-main hover:bg-opacity-40 "
+              >
+                <td className="text-left border-r-4 bg-secondary-main border-white px-1 sticky left-0">
+                  {cases.date}
+                </td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.positive}
+                </td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.positiveIncrease}
+                </td>
+                <td className="border-r-4 border-white px-1">{cases.death}</td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.deathIncrease}
+                </td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.totalTestResults}
+                </td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.totalTestResultsIncrease}
+                </td>
+                <td className="border-r-4 border-white px-1">
+                  {cases.hospitalizedCurrently}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      ) : (
+        <tbody>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
+            <tr key={item}>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+              <td>
+                <div className="animate-pulse h-6 bg-primary-light bg-opacity-50 w-full"></div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      )}
+    </table>
+  );
+}
+
+export default USTable;
