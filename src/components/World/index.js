586906971b95f41004501e6ec474a53a562cdce6
Luis Cristo <cristlui4@gmail.com>
2021-01-14 17:54:04 -0600

    added initial code for the world index page

--- src/components/World/index.js
+++ src/components/World/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import WorldTable from "./worldTable";
+import { DateTime } from "luxon";
 
-function Index() {
-  return <div>Page for the Worldometer data</div>;
+function WorldPage() {
+  return (
+    <div className="text-white">
+      <div className="text-white">Page for the Worldometer data</div>
+      <WorldTable />
+    </div>
+  );
 }
 
-export default Index;
+export default WorldPage;
