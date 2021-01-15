a7feceb2ebc92efb58fe13f8dc427285c96bf19a
Luis Cristo <cristlui4@gmail.com>
2021-01-15 14:25:36 -0600

    updated the intro component to reflect the new api used

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,19 +4,32 @@ function Intro() {
   return (
     <div className="container mx-auto px-2 sm:px-4">
       <div className="text-white">
-        This app currently consumes the API from{" "}
-        <a
-          href="https://covidtracking.com/"
-          aria-label="Go to API Source"
-          className="text-white italic underline"
-        >
-          The Covid Tracking Project
-        </a>
-        .
+        This app currently consumes data from both{" "}
+        <span>
+          <a
+            href="https://covidtracking.com/"
+            aria-label="Go to covid traking project api source"
+            className="text-white italic underline"
+          >
+            The Covid Tracking Project
+          </a>
+        </span>{" "}
+        and the{" "}
+        <span>
+          <a
+            href="https://disease.sh/"
+            aria-label="Go to disease.sh api Source"
+            className="text-white italic underline"
+          >
+            disease.sh
+          </a>
+        </span>{" "}
+        API.
       </div>
       <div className="pt-4">
         <p className="text-white">
-          Please select one of the options above to see the corresponding chart.
+          Please select one of the navigation options to see the corresponding
+          data.
         </p>
       </div>
     </div>
