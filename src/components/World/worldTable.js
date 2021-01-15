c71bd3d93d9e5c8334f6d8aaf6745473356eb003
Luis Cristo <cristlui4@gmail.com>
2021-01-15 16:40:37 -0600

    added icon indicator for the worldtable component

--- src/components/World/worldTable.js
+++ src/components/World/worldTable.js
@@ -7,40 +7,39 @@ function WorldTable({
   sortedField,
   handleTableHeaderClick,
 }) {
-  // const
-  //  checkSorting = (value) => {
-  //   if (sortedField && sortedField.key === value) {
-  //     if (sortedField.direction === "ascending") {
-  //       return (
-  //         <svg
-  //           focusable="false"
-  //           viewBox="0 0 24 24"
-  //           aria-hidden="true"
-  //           fill="#fff"
-  //           width="20"
-  //           height="20"
-  //         >
-  //           <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path>
-  //         </svg>
-  //       );
-  //     } else if (sortedField.direction === "descending") {
-  //       return (
-  //         <svg
-  //           focusable="false"
-  //           viewBox="0 0 24 24"
-  //           aria-hidden="true"
-  //           fill="#fff"
-  //           width="20"
-  //           height="20"
-  //         >
-  //           <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
-  //         </svg>
-  //       );
-  //     }
-  //   }
+  const checkSorting = (value) => {
+    if (sortedField && sortedField.key === value) {
+      if (sortedField.direction === "ascending") {
+        return (
+          <svg
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            fill="#fff"
+            width="20"
+            height="20"
+          >
+            <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path>
+          </svg>
+        );
+      } else if (sortedField.direction === "descending") {
+        return (
+          <svg
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            fill="#fff"
+            width="20"
+            height="20"
+          >
+            <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
+          </svg>
+        );
+      }
+    }
 
-  //   return null;
-  // };
+    return null;
+  };
 
   return (
     <table
@@ -55,70 +54,70 @@ function WorldTable({
             onClick={() => handleTableHeaderClick("country")}
           >
             Country
-            {/* {checkSorting("rawDate")} */}
+            {checkSorting("country")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("cases")}
           >
             Total Cases
-            {/* {checkSorting("positive")} */}
+            {checkSorting("cases")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("todayCases")}
           >
             New Cases
-            {/* {checkSorting("positiveIncrease")} */}
+            {checkSorting("todayCases")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("deaths")}
           >
             Total Deaths
-            {/* {checkSorting("death")} */}
+            {checkSorting("deaths")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("todayDeaths")}
           >
             New Deaths
-            {/* {checkSorting("deathIncrease")} */}
+            {checkSorting("todayDeaths")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("tests")}
           >
             Total Tests
-            {/* {checkSorting("totalTestResults")} */}
+            {checkSorting("tests")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("active")}
           >
             Active Cases
-            {/* {checkSorting("totalTestResultsIncrease")} */}
+            {checkSorting("active")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("critical")}
           >
             Serious, Critical
-            {/* {checkSorting("hospitalizedCurrently")} */}
+            {checkSorting("critical")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("recovered")}
           >
             Total Recovered
-            {/* {checkSorting("hospitalizedCurrently")} */}
+            {checkSorting("recovered")}
           </th>
           <th
             className="sticky top-0 bg-secondary-main px-2 cursor-pointer"
             onClick={() => handleTableHeaderClick("todayRecovered")}
           >
             Today Recovered
-            {/* {checkSorting("hospitalizedCurrently")} */}
+            {checkSorting("todayRecovered")}
           </th>
         </tr>
       </thead>
