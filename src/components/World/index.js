839f65184f8ca5ca1ff75c6225d629d78db58c99
Luis Cristo <cristlui4@gmail.com>
2021-01-15 16:34:26 -0600

    adjusted the data sorting for the world component

--- src/components/World/index.js
+++ src/components/World/index.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
-import WorldTable from "./worldTable";
 import { DateTime } from "luxon";
+import WorldTable from "./worldTable";
 
 function WorldPage() {
   const [totalCases, setTotalCases] = useState(null);
+  // const [dates,setDates]=useState();
   const [skeletonLoader, setSkeletonLoader] = useState(true);
 
   useEffect(() => {
@@ -26,15 +27,64 @@ function WorldPage() {
     }
     fetchWorldOMeter();
   }, []);
-  if (totalCases) {
-    console.log("data retrieved: ", totalCases);
-  }
+
+  // ****** //
+  const [sortedField, setSortedField] = useState(null);
+  const handleTableHeaderClick = (name) => {
+    requestSort(name);
+  };
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortedField &&
+      sortedField.key === key &&
+      sortedField.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortedField({ key, direction });
+  };
+  let sortedData = useMemo(() => {
+    if (totalCases && sortedField !== null) {
+      const data = [...totalCases];
+      data.sort((a, b) => {
+        if (a[sortedField.key] < b[sortedField.key]) {
+          return sortedField.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortedField.key] > b[sortedField.key]) {
+          return sortedField.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+      return data;
+    } else if (totalCases) {
+      const data = [...totalCases];
+      data.sort((a, b) => {
+        if (a["cases"] < b["cases"]) {
+          return 1;
+        }
+        if (a["cases"] > b["cases"]) {
+          return -1;
+        }
+        return 0;
+      });
+      return data;
+    } else {
+      return null;
+    }
+  }, [sortedField, totalCases]);
+
+  // ****** //
+
   return (
     <div className="text-white px-4 flex flex-wrap flex-col items-center max-w-max mx-auto mb-12">
       <WorldTable
         totalCases={totalCases}
-        sortedData={totalCases}
+        sortedData={sortedData}
         skeleton={skeletonLoader}
+        sortedField={sortedField}
+        handleTableHeaderClick={handleTableHeaderClick}
       />
       {/* )} */}
     </div>
