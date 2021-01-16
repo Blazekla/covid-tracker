d92521d08c84fd8e6ba82073e9cad98c416d1322
Luis Cristo <cristlui4@gmail.com>
2021-01-16 15:10:18 -0600

    added worldometer options to view yesterday and 2daysago data

--- src/components/World/index.js
+++ src/components/World/index.js
@@ -5,7 +5,7 @@ import WorldTable from "./worldTable";
 
 function WorldPage() {
   const [totalCases, setTotalCases] = useState(null);
-  // const [dates,setDates]=useState();
+  const [dates, setDates] = useState("");
   const [skeletonLoader, setSkeletonLoader] = useState(true);
 
   useEffect(() => {
@@ -14,7 +14,7 @@ function WorldPage() {
         setSkeletonLoader(true);
         setTotalCases(null);
         const data = await axios.get(
-          `https://disease.sh/v3/covid-19/countries`
+          `https://disease.sh/v3/covid-19/countries${dates}`
         );
         setSkeletonLoader(false);
         data.data.forEach((item) => {
@@ -26,7 +26,7 @@ function WorldPage() {
       }
     }
     fetchWorldOMeter();
-  }, []);
+  }, [dates]);
 
   // ****** //
   const [sortedField, setSortedField] = useState(null);
@@ -79,6 +79,32 @@ function WorldPage() {
 
   return (
     <div className="text-white px-4 flex flex-wrap flex-col items-center max-w-max mx-auto mb-12">
+      <div className="flex mb-8">
+        <button
+          className={`px-2 rounded-full ${
+            dates === "" ? "bg-primary-light" : null
+          }`}
+          onClick={() => setDates("")}
+        >
+          Now
+        </button>
+        <button
+          className={`px-2 rounded-full ${
+            dates === "?yesterday=true" ? "bg-primary-light" : null
+          }`}
+          onClick={() => setDates("?yesterday=true")}
+        >
+          Yesterday
+        </button>
+        <button
+          className={`px-2 rounded-full ${
+            dates === "?twoDaysAgo=true" ? "bg-primary-light" : null
+          }`}
+          onClick={() => setDates("?twoDaysAgo=true")}
+        >
+          2 Days Ago
+        </button>
+      </div>
       <WorldTable
         totalCases={totalCases}
         sortedData={sortedData}
