824eb48a5f2ddc4b6a4d0ec632f2486822ada9e7
Luis Cristo <cristlui4@gmail.com>
2021-01-21 07:52:26 -0600

    fixed worldometer page routes to be used with api

--- src/Worldometer.js
+++ src/Worldometer.js
@@ -16,12 +16,28 @@ function Worldometer() {
         >
           World Data
         </NavLink>
+        <NavLink
+          to={`${url}/world/usa`}
+          className="p-4 rounded-full border-solid border-2 border-primary-light text-white m-4"
+          activeClassName="underline bg-primary-light"
+          aria-label="See US Numbers"
+        >
+          US Data
+        </NavLink>
       </div>
+
       <Route
+        path={`${path}/world/usa/:state`}
+        render={(props) => <Country {...props} />}
+      />
+      <Route
+        exact
         path={`${path}/world/:country`}
         render={(props) => <Country {...props} />}
       />
-
+      <Route exact path={`${path}/world/usa`}>
+        {/* <World /> */}
+      </Route>
       <Route exact path={`${path}/world`}>
         <World />
       </Route>
