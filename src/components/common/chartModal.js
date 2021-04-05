c84c5b02e66850625cd5de7ab320f40998481cb4
Luis Cristo <cristlui4@gmail.com>
2021-04-05 11:11:26 -0500

    adjusted the horizontal margin for the chartModal component

--- src/components/common/chartModal.js
+++ src/components/common/chartModal.js
@@ -35,7 +35,7 @@ function ChartModal({
         className="bg-black bg-opacity-60 z-40 left-0 top-0 right-0 bottom-0 absolute"
         onClick={handleToggle}
       ></div>
-      <div className="flex flex-col items-center my-12 bg-secondary-dark z-50 px-2 py-8 sm:p-8">
+      <div className="flex flex-col items-center mx-4 sm:mx-8 my-12 bg-secondary-dark z-50 px-2 py-8 sm:p-8">
         <div className="flex flex-wrap justify-between w-full mb-8 p-2">
           <div className="text-white  flex flex-wrap justify-center">
             <div className="p-1">
