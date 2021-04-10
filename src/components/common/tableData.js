14efe6221c88d2d167ba1682b6f55cac4fa078ef
Luis Cristo <cristlui4@gmail.com>
2021-04-10 15:03:46 -0500

    refactored the covid api table body data to the tableDate component

---
+++ src/components/common/tableData.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function tableData({ objectData, data }) {
+  return (
+    <>
+      {objectData.map((cell) => {
+        if (cell.dataTitle === "rawDate") {
+          return (
+            <td
+              className="text-left border-r-4 bg-blue-900 border-white p-2 sticky left-0"
+              key={cell.dataTitle}
+            >
+              {data.date}
+            </td>
+          );
+        }
+        return (
+          <td
+            className="border-r-4 bg-pink-400 border-white p-2"
+            key={cell.dataTitle}
+          >
+            {data[cell.dataTitle] != null
+              ? data[cell.dataTitle].toLocaleString("en", {
+                  useGrouping: true,
+                })
+              : 0}
+          </td>
+        );
+      })}
+    </>
+  );
+}
+
+export default tableData;
